29e0bfe09a4736d11948854844c834f57b5c30a1
KonradWrbl <160815@stud.prz.edu.pl>
2021-11-05 14:14:06 +0100

    component added

--- libs/testapp/list/src/lib/testapp-list.module.ts
+++ libs/testapp/list/src/lib/testapp-list.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ListComponent } from './constainers/list/list.component';
 
 @NgModule({
   imports: [
@@ -8,6 +9,9 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
+  ],
+  declarations: [
+    ListComponent
   ],
 })
 export class TestappListModule {}
